690e639e0079eb76970d021011be2ce3f2eb8d69
daveronan <daveronan90@gmail.com>
2021-03-08 23:09:46 +0000

    Add user authentication to app

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,9 +1,19 @@
 import { shallow } from "enzyme";
 import React from "react";
-import Header from "../../components/Header";
+import { Header } from "../../components/Header";
+
+let wrapper, startLogout;
+
+beforeEach(() => {
+  startLogout = jest.fn();
+  wrapper = shallow(<Header startLogout={startLogout} />);
+});
 
 test("Should render header correctly", () => {
-  const wrapper = shallow(<Header />);
-
   expect(wrapper).toMatchSnapshot();
+});
+
+test("Should call startLogout on button click", () => {
+  wrapper.find("button").simulate("click");
+  expect(startLogout).toHaveBeenCalled();
 });
